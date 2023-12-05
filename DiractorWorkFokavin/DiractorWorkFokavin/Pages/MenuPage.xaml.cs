cd98d923c60aa10395a771b12feff06438f80c3e
ИС201-1 <IS201-1@tisbi.ru>
2023-12-05 16:16:58 +0300

    Hello

---
+++ DiractorWorkFokavin/DiractorWorkFokavin/Pages/MenuPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DiractorWorkFokavin.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для MenuPage.xaml
+    /// </summary>
+    public partial class MenuPage : Page
+    {
+        public MenuPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Комедия_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Ужасы_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Драма_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Боевик_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
